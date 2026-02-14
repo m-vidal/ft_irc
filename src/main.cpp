2d082dc34b3e9216d13b6d0ccf34bf5abb64a539
welepy-dev <mwsilva.contato@gmail.com>
2026-02-14 14:47:35 +0100

    main basico

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: mvidal <mvidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/30 17:55:06 by marcsilv          #+#    #+#             */
-/*   Updated: 2026/02/05 17:43:46 by marcsilv         ###   ########.fr       */
+/*   Created: 2026/01/27 09:19:53 by mvidal            #+#    #+#             */
+/*   Updated: 2026/02/14 14:35:56 by marcsilv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/Parser.hpp"
+#include <stdexcept>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+#include <cstdlib>
+#include <sstream>
 
-int main(int ac, char **av) {
-    if (ac < 2) {
-        std::cerr << "Usage: " << av[0] << " <argument>\n";
-        return 1;
-    }
+#include "../inc/Server.hpp"
 
-    std::string str = av[1];
-    User u;
-    Parser a(u, str);
-    return 0;
+int main(int ac, char** av) {
+    std::string		password;
+    unsigned short	port;
+    char			c;
+
+	if (ac != 3)
+        return (std::cerr << "Error: bad arguments." << std::endl, 1);
+
+	std::stringstream ss(av[1]);
+	if (!(ss >> port) || ss >> c) {
+		return (std::cerr << "Error: invalid port." << std::endl, 1);
+	}
+	//task: verificar o range da password [1024;65536]
+	std::cout << port << std::endl;
+	password = av[2];
+	try {
+		Server server(port, password);
+	} catch (std::runtime_error &e) {
+		std::cout << e.what() << std::endl;
+	}
+	//task: verificar password vazia
+
+	/*int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		return(std::cout << "Socket Not Created!" << std::endl, 1);
+	}
+	std::cout << "Socket fd: " << sockfd << std::endl;
+	close(sockfd);*/
+	return (0);
 }
