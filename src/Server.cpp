f34c7866570ed6a87adfd43dcd1a7e558463e9b8
Marcio Vidal <mvidal@c2r3s2.42luanda.com>
2026-02-14 20:11:36 +0100

    modificando a ordem de inicialização dos membros const iniciando a validação do socket criado descomentando o destrutor

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,30 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: mvidal <mvidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 14:33:23 by marcsilv          #+#    #+#             */
-/*   Updated: 2026/02/14 14:45:35 by marcsilv         ###   ########.fr       */
+/*   Updated: 2026/02/14 20:07:52 by mvidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Server.hpp"
 
 Server::Server(unsigned short &port, std::string &password): 
-_socket(socket(AF_INET, SOCK_STREAM, 0)),
-_port(port),
-_password(password) {
-
+_password(password), _socket(socket(AF_INET, SOCK_STREAM, 0)), _port(port) {
 	//configurar socket
+	if (_socket < 0)
+		throw std::runtime_error("Error in socket creation!");
 	//"bind" socket
+	
 	//ficar a espera de eventos
 	//configurar comportamento quando há uma nova conexão
 	//processar linhas vindas do cliente
 	//para cada linha completa instanciar o parser
-
+	is_running = true;
 }
 
 Server::~Server(void) {
-	/*if (_socket)
-		close(_socket);*/
+	if (_socket)
+		close(_socket);
 }
