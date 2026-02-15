13b232770a055d2d59e7911e59365e1e8598b586
Marcio Vidal <mvidal@c2r1s10.42luanda.com>
2026-02-15 03:59:55 +0100

    adicionando o bind para finalmente asociar o nosso socket a uma porta

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: mvidal <mvidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 14:33:23 by marcsilv          #+#    #+#             */
-/*   Updated: 2026/02/15 03:44:28 by mvidal           ###   ########.fr       */
+/*   Updated: 2026/02/15 03:58:49 by mvidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@ Server::Server(unsigned short &port, std::string &password):
 _password(password), _socket(socket(AF_INET, SOCK_STREAM, 0)), _port(port) {
 	(void)_port;
 	if (_socket < 0)
-		throw std::runtime_error("Error fail in socket server creation!");
+		throw std::runtime_error("Error failure in the socket server creation!");
 
 	int	opt = 1;
 	if (setsockopt(_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
-		throw std::runtime_error("Error: fail in socket server config!");	
+		throw std::runtime_error("Error: failure in the socket server config!");	
 	
 	_addr.sin_family = AF_INET;
 	_addr.sin_port = htons(port);
 	_addr.sin_addr.s_addr = INADDR_ANY;
 
+	if (bind(_socket, reinterpret_cast<struct sockaddr *>(&_addr), sizeof(_addr)))
+		throw std::runtime_error("Error: failure to associate the port.");
 	is_running = false;
 }
 
