2305523c192f1d96459ff9812e7e6ff804cef78e
Marcio Vidal <mvidal@c3r6s7.42luanda.com>
2026-02-28 16:29:41 +0100

    adicionando o atributo stepOne para marcar a primeira parte da autenticacao do user (a password)

--- inc/User.hpp
+++ inc/User.hpp
@@ -6,7 +6,7 @@
 /*   By: mvidal <mvidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 17:54:53 by marcsilv          #+#    #+#             */
-/*   Updated: 2026/02/28 13:55:30 by mvidal           ###   ########.fr       */
+/*   Updated: 2026/02/28 16:23:19 by mvidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ class User {
 		//std::vector<Channel>	_channels;	//acho que se vai apagar depois
 		std::string				_nickname;
 		std::string 			_username;
+		bool					_stepOne;
 		std::string				_buffer;
 		const short				_fd;
 
@@ -43,15 +44,17 @@ class User {
 		std::string				getBuffer( void );
 		size_t					getInteractions();
 		bool					isAuthenticated();
+		bool					getStepOne();
 		int						getFd(void) ;
 
 		// setters
 		void					setNick(const std::string &nick);
 		void					setUser(const std::string &user);
 		void					appendToBuffer(std::string data);
-
+		
 		void					incInteractions();
 		void					authenticate();
+		void					setStepOne();
 };
 
 #endif
--- src/User.cpp
+++ src/User.cpp
@@ -6,67 +6,51 @@
 /*   By: mvidal <mvidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 17:55:00 by marcsilv          #+#    #+#             */
-/*   Updated: 2026/02/28 14:51:57 by mvidal           ###   ########.fr       */
+/*   Updated: 2026/02/28 16:28:23 by mvidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/User.hpp"
 
-User::User(int fd): _isAuthenticated(false),  _interactions(0), _fd(fd)
+User::User(int fd): _isAuthenticated(false),  _interactions(0), _stepOne(false), _fd(fd)
 { _username = "Marcio Vidal"; _nickname = "mvidal"; }
 
-User::User(void): _isAuthenticated(false),  _interactions(0), _fd(-2)
+User::User(void): _isAuthenticated(false),  _interactions(0), _stepOne(false), _fd(-2)
 { _username = "default"; _nickname = "default";  }
 
 User::~User(void) { }
 
 
-void User::setNick(const std::string &nick) {
-	this->_nickname = nick;
-}
+void User::setNick(const std::string &nick) { this->_nickname = nick; }
 
-void User::setUser(const std::string &user) {
-	this->_username = user;
-}
-void	User::incInteractions()
-{
-	_interactions++;
-}
+void User::setUser(const std::string &user) {this->_username = user; }
 
-size_t	User::getInteractions( void ) {
-	return (_interactions);
-}
+
+size_t	User::getInteractions( void ) { return (_interactions); }
+void	User::incInteractions() { _interactions++; }
 
 
 bool	User::isAuthenticated() { return (_isAuthenticated); }
-
 void	User::authenticate() { _isAuthenticated = true; }
 
 
-const std::string &User::getNick(void) const {
-	return (this->_nickname);
-}
+const std::string	&User::getUsername(void) const { return (this->_username); }
 
-const std::string &User::getUsername(void) const {
-	return (this->_username);
-}
+const std::string	&User::getNick(void) const { return (this->_nickname); }
 
-bool User::isOperator(void) const {
-	return (this->_isOperator);
-}
 
-void	User::appendToBuffer(std::string data) {
-	_buffer += data;
-}
+bool User::isOperator(void) const { return (this->_isOperator); }
 
-std::string	User::getBuffer( void ) {
-	return (_buffer);
-}
 
-void	User::clearBuffer( size_t pos ) {
-	_buffer.erase(0, pos);
-}
+void	User::appendToBuffer(std::string data) { _buffer += data; }
 
-//void	User::setFd(int newFd) { _fd = newFd; }
+void	User::clearBuffer( size_t pos ) { _buffer.erase(0, pos); }
+
+std::string	User::getBuffer( void ) { return (_buffer); }
+
 
 int	User::getFd(void) { return (_fd); }
+
+
+bool	User::getStepOne() { return(_stepOne); }
+void	User::setStepOne() { _stepOne = true; }
